a660c477ac2ed27e56875ecda07c39c3f53fb8e8
EduardoBecerra <79186035+EduardoBecerra@users.noreply.github.com>
2022-07-14 14:53:01 -0700

    first commit

---
+++ PhonePortal/PhonePortal/src/main/java/com/example/PhonePortal/PhonePortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhonePortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonePortalApplication.class, args);
+	}
+
+}
---
+++ PhonePortal/PhonePortal/src/test/java/com/example/PhonePortal/PhonePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhonePortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
